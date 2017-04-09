b860bc79a00a12576bb598ca4813299d7c50f59f
PhoenixChen98 <q867198081@gmail.com>
2017-04-09 16:40:46 +0800

    初步

---
+++ 智能花盆/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  智能花盆
+//
+//  Created by Phoenix on 2017/4/5.
+//  Copyright © 2017年 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 智能花盆/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  智能花盆
+//
+//  Created by Phoenix on 2017/4/5.
+//  Copyright © 2017年 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
